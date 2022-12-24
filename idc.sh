0ad944940cbfd8d5a44595fa6ba11a707a2519f2
mkp95 <mkp95@users.noreply.github.com>
2022-12-24 17:32:59 +0530

    custom commands updated

--- idc.sh
+++ idc.sh
@@ -1,27 +1,9 @@
-#!/system/bin/sh
-path=/data/system/users/0/settings_ssaid.xml
+package=com.example.app
 
-grep -o -n 'package="[a-zA-Z\.]*"*' $path
-printf "\nSelect package : "
-read p_num
-printf "\nCurrent ID is : "
-curr_id=$(sed -n "$p_num"p $path | grep -o 'value="[a-zA-Z0-9]*"*' | cut -d '"' -f2)
-printf $curr_id
 
-printf "\n\n1)Custom ID\n2)Random ID\nEnter choice : "
-read ch
-case $ch in
-  1 )
-    printf "\nEnter new ID : "
-    read new_id
-    sed -i -e "s/$curr_id/$new_id/g" $path
-    printf "\nNew ID set!\n"
-    ;;
-  2 )
-    printf "\nGenerating random ID..."
-    new_id=$(dd if=/dev/urandom count=256 bs=1 | tr -dc 'a-z0-9' | fold -w 16 | head -n 1)
-    printf "\nNew ID is : $new_id"
-    sed -i -e "s/$curr_id/$new_id/g" $path
-    printf "\nNew id set!\n"
-    ;;
-esac
+file=/data/system/users/0/settings_ssaid.xml
+curr_id=$(grep "package=\"$package\"" $file | grep -o 'value="[a-zA-Z0-9]*"*' | cut -d '"' -f2)
+echo $curr_id
+new_id=$(cat /dev/urandom count=256 bs=5 | tr -dc 'a-f0-9' | head -n 1 -c 16 )
+echo $new_id
+sed -i -e "s/$curr_id/$new_id/g" $file
